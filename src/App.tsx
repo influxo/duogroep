67592409e3a4497118b94ad886ec4b6653c227d2
ArtisAdemi <artis.ademi@gmail.com>
2025-03-29 22:06:22 +0100

    App with tailwind initialized

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="text-4xl font-bold text-blue-600">
+      Hello World
+    </div>
+  )
+}
+
+export default App
